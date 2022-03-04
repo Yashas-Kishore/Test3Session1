4e0d7516519838f39bb72db3ae83ba19a01ce552
Yashas Kishore <95675075+Yashas-Kishore@users.noreply.github.com>
2022-03-04 03:48:23 +0000

    1

--- p1original.c
+++ p1original.c
@@ -1,2 +1,20 @@
+#include<stdio.h>
 
+void input(int *num1, int *den1, int *num2, int *den2)
+{
+  printf("enter the fraction as a and b as in a/b :\n");
+  scanf("%d%d",num1,den1);
+  printf("enter the fraction as a and b as in a/b :\n");
+  scanf("%d%d",num2,den2);
 }
+
+int LCM(int x,int y)
+{
+  
+}
+
+void add(int num1,int den1, int num2, int den2, int *num3, int *den3)
+{
+  
+}
+void output(int num1, int den1, int num2, int den2, int num3, int den3);
