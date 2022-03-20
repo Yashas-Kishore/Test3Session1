116b752b47f20e735f5e6b5797c2b34334c51096
Yashas Kishore <95675075+Yashas-Kishore@users.noreply.github.com>
2022-03-20 19:21:09 +0000

    testing

--- p6original.c
+++ p6original.c
@@ -4,7 +4,7 @@
 void input_string(char *a)
 {
   printf("enter the string to check: \n");
-  scanf("%s",a);
+  scanf("%[^\n]s",a);
 }
 int count_words(char *string)
 {
@@ -15,20 +15,39 @@ int count_words(char *string)
   for(i=0;test;i++)
     {
       x=x+1;
+      test=strtok(NULL," ,.-!?");
     }
   return x;
 }
 void output(char *string, int no_words)
 {
-  printf("the number of words in the sentence/s %s is %d",string,no_words);
+ //  while(string != NULL)
+ // {
+ //   printf("%s ",string);
+ //   string=strtok(NULL," ,.-!?");
+ // }
+ printf("the number of words in the sentence\n");
+ printf("is %d\n",no_words);
 }
 
 int main()
 {
-  char a;
-  int b;
-  input_string(&a);
-  b= count_words(&a);
-  output(&a,b);
+  char a[1000];
+  // char b[100];
+  int c;
+  input_string(a);
+  // strcpy(b,a);
+  c= count_words(a);
+  output(a,c);
   return 0;
-}
+  }
+// ok sstiring is r an array of characters
+// you are not reading suprabatham
+// thats why prcatice
+
+// while(test != NULL)
+// {
+//    printf("%s ",test)
+//    test=strtok(NULL," ,.-!?");
+ //ok sir
+
